ce2d18a91e005e52e51f31432384a80283890159
Cal Henderson <cal@iamcal.com>
2016-06-23 12:03:15 -0500

    moved css to an external file added a basic bootstrap layout

--- www/index.php
+++ www/index.php
@@ -19,6 +19,7 @@
 	<title>timezone.help</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<link rel="stylesheet" href="/style.css">
 
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -61,60 +62,66 @@ $(function(){
 });
 
 </script>
-<style>
-
-#main {
-	width: 600px;
-	margin: 0 auto;
-	text-align: center;
-	font-family: Arial, sans-serif;
-}
-
-#detect {
-	display: none;
-	border-top: 2px solid #666;
-	border-bottom: 2px solid #666;
-	background-color: #eee;
-	padding: 1em;
-}
-
-#guess-name {
-	font-size: 130%;
-	margin: 0.5em;
-}
-
-#zone {
-	font-size: 80%;
-	margin-bottom: 0.5em;
-}
-
-#label-reg, #label-dst {
-	font-weight: bold;
-}
-
-</style>
 </head>
 <body>
 
-<div id="main">
+<div class="container">
+	<div class="header clearfix">
+		<nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="/">Home</a></li>
+            <li role="presentation"><a href="/about">About</a></li>
+            <li role="presentation"><a href="https://github.com/iamcal/lib_timezones">Source Code</a></li>
+          </ul>
+		</nav>
+		<h3 class="text-muted">timezone.help</h3>
+	</div>
 
-	<h1>timezone.help</h1>
+	<div class="jumbotron" id="detect">
 
-	<div id="detect">
 		We have detected your timezone as:
-		<div id="guess-name"></div>
+		<h1 id="guess-name"></h1>
 		<div id="zone">(Zone ID: <span id="guess-zone"></span>)</div>
 		The label for this timezone is <span id="label-reg"></span><br />
 		<span id="dst-yes">During daylight savings, the label is <span id="label-dst"></span><br /></span>
 		<span id="dst-no">This timezone does not observe daylight savings.<br /></span>
 	</div>
 
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
 	<p>
 		<a href="https://github.com/iamcal/lib_timezones">Source on GitHub</a><br />
-		Git rev: <?php echo $sha; ?>
+		Git rev: <?php echo substr($sha, 0, 10); ?>
 	</p>
 
-</div>
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+		<footer class="footer">
+			<p>&copy; 2013-<?php echo date('Y'); ?> Cal Henderson</p>
+		</footer>
+
+    </div>
+
+
+
 
 </body>
 </html>
