ced82a02c75e5157b68f7cc6d890e769894f863c
Cal Henderson <cal@iamcal.com>
2016-06-24 13:32:40 -0500

    point to the latest paths

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-	include('lib_timezones/lib_timezones.php');
+	include('lib_timezones/lib/lib_timezones.php');
 
 	chdir(dirname(__FILE__).'/lib_timezones');
 	$sha = trim(shell_exec('git rev-parse HEAD'));
@@ -28,7 +28,7 @@
 
 	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-	<script src="/lib_timezones/lib_timezones.js"></script>
+	<script src="/lib_timezones/lib/lib_timezones.js"></script>
 <script>
 
 var zonedata = <?php echo json_encode($list); ?>;
